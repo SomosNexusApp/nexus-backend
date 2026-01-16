ea7343b1af1f0ed9420ad58f20ff871cc19aa337
Juan José Gamero López <jgamlop2101@g.educaand.es>
2026-01-16 13:36:22 +0100

    Realizado controlador para Usuario

---
+++ src/main/java/com/nexus/controller/UsuarioController.java
@@ -0,0 +1,85 @@
+package com.nexus.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nexus.entity.Usuario;
+import com.nexus.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	// Obtener todos los usuarios
+	public List<Usuario> getAllUsuarios() {
+		return usuarioService.findAll();
+	}
+	
+	
+	// Obtener usuario por id
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getUsuarioById(@PathVariable Integer id) {
+		Optional<Usuario> usuarioOptional = usuarioService.findById(id);
+		
+		if (usuarioOptional.isPresent()) {
+			return ResponseEntity.ok(usuarioOptional.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	
+	// Crear usuario
+	@PostMapping
+	public ResponseEntity<Usuario> createUsuario(@RequestBody Usuario usuario) {
+		Usuario nuevoUsuario = usuarioService.save(usuario);
+		return ResponseEntity.ok(nuevoUsuario);
+	}
+	
+	
+	// Actualizar usuario
+	@PutMapping("/{id}")
+	public ResponseEntity<Usuario> updateUsuario(@PathVariable Integer id, @RequestBody Usuario usuarioDetalles) {
+		Optional<Usuario> usuarioOptional = usuarioService.findById(id);
+		
+		if (usuarioOptional.isPresent()) {
+			Usuario usuarioExistente = usuarioOptional.get();
+			usuarioExistente.setUser(usuarioDetalles.getUser());
+			usuarioExistente.setEmail(usuarioDetalles.getEmail());
+			
+			Usuario usuarioActualizado = usuarioService.save(usuarioExistente);
+			return ResponseEntity.ok(usuarioActualizado);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	
+	// Eliminar usuario
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteUsuario(@PathVariable Integer id) {
+		Optional<Usuario> usuarioOptional = usuarioService.findById(id);
+		
+		if (usuarioOptional.isPresent()) {
+			usuarioService.delete(id);
+			return ResponseEntity.ok("Usuario eliminado correctamente...");
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No se ha encontrado el usuario para eliminar");
+		}
+	}
+}
